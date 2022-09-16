8f62d1aca47036d445a3153d8bd56eb8fd57fb1e
nico britz <nico.britz@outlook.com>
2022-09-16 16:29:43 +0100

    update readme and fix internal date parsing

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { Box, Flex, Input, Button, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { FC, useState } from "react";
 import NextImage from "next/image";
@@ -34,30 +34,46 @@ const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
       <Flex justify="center" align="center" height="calc(100vh - 100px)">
         <Box padding="50px" bg="gray.900" borderRadius="6px">
           <form onSubmit={handleSubmit}>
-            <Input
-              placeholder="firstname"
-              type="text"
-              onChange={(e) => setFirstname(e.target.value)}
-            />
-            <Input
-              placeholder="lastname"
-              type="text"
-              onChange={(e) => setLastname(e.target.value)}
-            />
+            {mode === "signup" ? (
+              <Text fontSize="2xl">Sign Up</Text>
+            ) : (
+              <Text fontSize="2xl">Sign In</Text>
+            )}
+
+            {mode === "signup" && (
+              <>
+                <Input
+                  placeholder="firstname"
+                  type="text"
+                  onChange={(e) => setFirstname(e.target.value)}
+                  marginY="10px"
+                />
+                <Input
+                  placeholder="lastname"
+                  type="text"
+                  onChange={(e) => setLastname(e.target.value)}
+                  marginY="10px"
+                />
+              </>
+            )}
+
             <Input
               placeholder="email"
               type="email"
               onChange={(e) => setEmail(e.target.value)}
+              marginY="10px"
             />
             <Input
               placeholder="password"
               type="password"
               onChange={(e) => setPassword(e.target.value)}
+              marginY="10px"
             />
             <Button
               type="submit"
               bg="green.500"
               isLoading={isLoading}
+              marginY="10px"
               sx={{ "&:hover": { bg: "green.300" } }}
             >
               {mode}
--- components/playerBar.tsx
+++ components/playerBar.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import { useStoreState } from "easy-peasy";
 import Player from "./player";
 
@@ -11,8 +12,21 @@ const PlayerBar = () => {
       <Flex align="center">
         {activeSong ? (
           <Box padding="20px" color="white" width="30%">
-            <Text fontSize="large">{activeSong.name}</Text>
-            <Text fontSize="sm">{activeSong.artist.name}</Text>
+            <Flex>
+              <Box>
+                <Image
+                  boxSize="50px"
+                  boxShadow="2xl"
+                  src={`https://picsum.photos/400?random=${activeSong.name}`}
+                  borderRadius="3px"
+                  marginX="20px"
+                />
+              </Box>
+              <Box>
+                <Text fontSize="large">{activeSong.name}</Text>
+                <Text fontSize="sm">{activeSong.artist.name}</Text>
+              </Box>
+            </Flex>
           </Box>
         ) : null}
         <Box width="40%">
